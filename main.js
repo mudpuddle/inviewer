5adaf3ab9cde8c4585ab27a58298822b7d5b56f2
Jesse W <jesse.wegner@gmail.com>
2015-10-02 12:15:36 -0700

    removed project specfic url

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ var configuration = require('./configuration');
 var ipc = require('ipc');
 
 app.on('ready', function() {
+  var w = 650;
+  var h = 510;
   var mainWindow = new BrowserWindow({
-    width: 650,
-    height: 510,
+    width: (process.platform == 'win32') ? w + 15 : w,
+    height: (process.platform == 'win32') ? h + 15 : h,
     show: false,
     "node-integration": false
   });
 
-  mainWindow.loadUrl('https://invis.io/9F4E39AK4');
+  mainWindow.loadUrl('http://www.github.com');
   mainWindow.show();
   mainWindow.webContents.on('did-finish-load', function() {
     mainWindow.webContents.insertCSS(".toolbar{display: none;} .powered-by{display: none;} .button-right{display: none;} .button-left{display: none;}")
